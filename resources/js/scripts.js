c2be3bf16d2c9214df111525b993eff8d388db0f
Scott Eifel <scotte@reagan.com>
2021-05-01 13:56:13 -0400

    jquery effects on page elements finished, mobile navigation finished

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $(".js--section-features").waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
+            $('mobile-nav-icon').css
         } else {
             $('nav').removeClass('sticky');
         }
@@ -12,12 +13,58 @@ $(document).ready(function() {
             offset: '60px;'
     });
 
-    // Scroll on buttons
-    $('.js--scroll-to-plan').click(function() {
-        $('html,body').animate({scrollTop: $(".js--section-plans").offset().top}, 1000);
+    // Using smooth scroll in css instead of jquery code below from lecture.
+
+    // // Scroll on buttons
+    // $('.js--scroll-to-plan').click(function() {
+    //     $('html,body').animate({scrollTop: $(".js--section-plans").offset().top}, 1000);
+    // });
+
+    // $('.js--scroll-to-start').click(function() {
+    //     $('html,body').animate({scrollTop: $(".js--section-features").offset().top}, 1000);
+    // });
+
+    // Animations on scroll
+    $(".js--wp-1").waypoint(function(direction) {
+        $(".js--wp-1").addClass("animate__animated animate__fadeIn");
+    }, {
+            offset: '50%'
     });
 
-    $('.js--scroll-to-start').click(function() {
-        $('html,body').animate({scrollTop: $(".js--section-features").offset().top}, 1000);
+    $(".js--wp-2").waypoint(function(direction) {
+        $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    }, {
+            offset: '50%'
     });
+
+    $(".js--wp-3").waypoint(function(direction) {
+        $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    }, {
+            offset: '50%'
+    });
+
+    $(".js--wp-4").waypoint(function(direction) {
+        $(".js--wp-4").addClass("animate__animated animate__pulse");
+    }, {
+            offset: '50%'
+    });
+
+    // Mobile navigation
+
+    $('.js--nav-icon').click(function() {
+        const nav = $('.js--main-nav');
+        const icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    })
+
 });
